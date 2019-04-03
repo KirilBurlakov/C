f975dea2b857c8264f3e0a1942f56b512ef44669
Kiril Burlakov <burlakovkiril19@gmail.com>
2019-04-03 17:27:32 +0200

    New Version

--- Lab2/main.c
+++ Lab2/main.c
@@ -3,6 +3,23 @@
 
 int main()
 {
+
+    char* pSource;
+    char* pDestination;
+    pSource = (char*)calloc(1024, sizeof(char));
+    pDestination = (char*)malloc(1024);
+
+    int i;
+    for(i = 0; i < 100; i++)
+    {
+        pSource[i] = i;
+    }
+
+    for(i = 0; i < 100; i++)
+    {
+        pDestination[i] = pSource[i];
+        printf("%d ", pDestination[i]);
+    }
     printf("Hello world!\n");
     return 0;
 }
