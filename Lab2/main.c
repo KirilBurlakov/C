e234cc114a405751d6f6acb7bcd355d3b64475ea
Kiril Burlakov <burlakovkiril19@gmail.com>
2019-04-03 18:09:45 +0200

    Second Commit, added comments

--- Lab2/main.c
+++ Lab2/main.c
@@ -3,23 +3,37 @@
 
 int main()
 {
-
+    //Allocate static memory
     char* pSource;
     char* pDestination;
     pSource = (char*)calloc(1024, sizeof(char));
     pDestination = (char*)malloc(1024);
 
+
+    //Initialize the first array with some values
     int i;
-    for(i = 0; i < 100; i++)
+    for(i = 0; i < 1024; i++)
     {
         pSource[i] = i;
     }
 
-    for(i = 0; i < 100; i++)
+
+    //Copy the values from the source to destination
+    //Start measuring time here
+    //alt_timestamp_start()
+    //alt_u64 start = alt_timestamp_start();
+    for(i = 0; i < 1024; i++)
     {
         pDestination[i] = pSource[i];
         printf("%d ", pDestination[i]);
     }
-    printf("Hello world!\n");
+    //alt_u64 finish = alt_timestamp_start();
+    //printf("%d", ((finish - start)/alt_timestamp_freq()))
+    //Finish measuring
+
+    free(pSource);
+    free(pDestination);
+
+
     return 0;
 }
