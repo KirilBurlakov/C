1d8a5a6978dc4b94d134f586e8776addb7f9dfee
Kiril Burlakov <burlakovkiril19@gmail.com>
2019-04-07 20:45:17 +0200

    Updated memory allocation

--- Lab2/main.c
+++ Lab2/main.c
@@ -1,18 +1,31 @@
 #include <stdio.h>
 #include <stdlib.h>
+#define NUM_BYTES 1024
+
 
 int main()
 {
-    //Allocate static memory
-    char* pSource;
-    char* pDestination;
-    pSource = (char*)calloc(1024, sizeof(char));
-    pDestination = (char*)malloc(1024);
+    /*TODO:
+        1) Create the instances for 8, 16, 32, 64 - bit vars
+        2) Use Altera vars
 
+    */
+
+
+
+    //Allocate static memory
+    int* pSource;
+    int* pDestination;
+
+
+    pSource = (int*)malloc(NUM_BYTES);
+    pDestination = (int*)malloc(NUM_BYTES);
+
+    //printf("%d", sizeof(pSource));
 
     //Initialize the first array with some values
     int i;
-    for(i = 0; i < 1024; i++)
+    for(i = 0; i < NUM_BYTES/sizeof(int); i++)
     {
         pSource[i] = i;
     }
@@ -22,12 +35,12 @@ int main()
     //Start measuring time here
     //alt_timestamp_start()
     //alt_u64 start = alt_timestamp_start();
-    for(i = 0; i < 1024; i++)
+    for(i = 0; i < NUM_BYTES/sizeof(int); i++)
     {
         pDestination[i] = pSource[i];
-        printf("%d ", pDestination[i]);
+        printf("%d", pDestination[i]);
     }
-    //alt_u64 finish = alt_timestamp_start();
+    //alt_u64 finish = alt_timestamp();
     //printf("%d", ((finish - start)/alt_timestamp_freq()))
     //Finish measuring
 
